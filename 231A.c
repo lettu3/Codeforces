4339529998cc81aa57925315c5faad8d09fcfef2
Ignacio Gomez Barrios <117108736+lettu3@users.noreply.github.com>
2025-03-26 19:42:59 -0300

    231A accepted

--- 231A.c
+++ 231A.c
@@ -4,30 +4,18 @@
 #include <stdio.h>
 
 void get_input_n(int * n){
-    fprintf(stdout, "Enter the value of n: ");
     fscanf(stdin, "%i", n);
     return;
 };
 
+
 void get_matrix(int row, int column, unsigned int arr[row][column]){
     for (int i = 0; i < row; i++){
-        fprintf(stdout, "Enter the elements of row %i: \n", i);
-        for (int j = 0; j < column; j++){
-            fscanf(stdin, "%u", &arr[i][j]);
-        }
+        fscanf(stdin,"%u %u %u", &arr[i][0], &arr[i][1], &arr[i][2]); 
     }
 
 };
 
-void print_matrix(int row, int column, unsigned int arr[row][column]){
-    for (int i = 0; i < row; i++){
-        for (int j = 0; j < column; j++){
-            fprintf(stdout, "%i ", arr[i][j]);
-        }
-        fprintf(stdout, "\n");
-    }
-    return;
-};
 
 int solve(int row, int column, unsigned int arr[row][column]){
     unsigned int solutions = 0;
@@ -55,9 +43,7 @@ int main (int argc, char** argv){
 
     unsigned int array[n][3];
 
-    get_matrix(n, 3, array);
-    print_matrix(n, 3, array);
-    
+    get_matrix(n, 3, array);    
     solution = solve(n, 3, array);
     fprintf(stdout, "%i\n", solution);
     return 0;                                                                  
