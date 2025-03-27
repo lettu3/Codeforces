6aa95aceb6c57b928f5dd57a4337990bcf6cb0f1
Ignacio Gomez Barrios <117108736+lettu3@users.noreply.github.com>
2025-03-27 13:49:12 -0300

    Accepted: #4/A

---
+++ 4A.c
@@ -0,0 +1,27 @@
+//gcc -Wall -std=c99 4A.c -o 4A
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void get_input(int * n){
+    fscanf(stdin,"%i", n);
+    return;
+};
+
+bool solve(int w){
+    return (w % 2 == 0);
+};
+
+int main(){
+    int w;
+    get_input(&w);
+    bool solution = solve(w);
+    if (solution){
+        fprintf(stdout,"YES\n");
+    }
+    else{
+        fprintf(stdout, "NO\n");
+    }
+    return 0;
+};
+
